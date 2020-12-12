a6b53253ee7d8e194914f51f9257b5e8f49faeb3
IronMan <73984830+Uday0011@users.noreply.github.com>
2020-12-12 10:11:43 +0530

    Update fun.py

--- SaitamaRobot/modules/fun.py
+++ SaitamaRobot/modules/fun.py
@@ -83,11 +83,10 @@ def slap(update: Update, context: CallbackContext):
     hit = random.choice(fun_strings.HIT)
     throw = random.choice(fun_strings.THROW)
 
-    if update.effective_user.id == 1096215023:
-       update.effective_message.reply_text(
-                    "f.",
-                    reply_to_message_id=reply)
-    
+    if update.effective_user.id == 1410092658:
+        temp = ".... scratches {user2}"
+        
+
     reply = temp.format(
         user1=user1, user2=user2, item=item, hits=hit, throws=throw)
 
