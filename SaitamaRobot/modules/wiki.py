bf17425dcc22566f9c910f9043d7de08bb19bfda
Saksham Singh <68587810+sakshameng@users.noreply.github.com>
2020-12-18 20:03:27 +0530

    Update wiki.py

--- SaitamaRobot/modules/wiki.py
+++ SaitamaRobot/modules/wiki.py
@@ -45,5 +45,5 @@ def wiki(update: Update, context: CallbackContext):
                 disable_web_page_preview=True)
 
 
-WIKI_HANDLER = DisableAbleCommandHandler("web", pedia)
+WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
 dispatcher.add_handler(WIKI_HANDLER)
